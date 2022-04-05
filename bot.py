0367f0d46f9faf891c542f485be4f7b8cabf9184
hunterpack <hpack55@vt.edu>
2022-04-05 15:34:27 -0400

    update code to use requests

---
+++ bot.py
@@ -0,0 +1,48 @@
+import os
+
+from fetch_data import fetch_price, check_map
+from discord.ext import commands
+
+
+def main():
+
+    bot = commands.Bot(command_prefix='!')
+
+    TOKEN = os.getenv('DISCORD_TOKEN')
+
+    @bot.event
+    async def on_ready():
+        print(f'{bot.user.name} has connected to Discord!')
+
+    @bot.command(name='price')
+    async def get_price(ctx, symbol):
+        try:
+            id_map = check_map(symbol.upper())
+
+            if len(id_map) > 1:
+                options = {}
+                for ticker in id_map:
+                    options[ticker["name"]] = ticker["id"]
+
+                await ctx.send(f"Please choose one of the following options: {', '.join(options.keys())}")
+
+                message_response = await bot.wait_for(
+                    'message', check=lambda m: m.author == ctx.author)
+
+                token_name = message_response.content
+                options = {k.lower(): v for k, v in options.items()}
+                ticker_id = str(options[token_name.lower()])
+                response = fetch_price(id=ticker_id)
+                await ctx.send(f"{token_name}'s current price is ${response}")
+            else:
+                ticker = symbol.upper()
+                response = fetch_price(symbol=ticker)
+                await ctx.send(f"{ticker}'s current price is ${response}")
+        except Exception as e:
+            await ctx.send(f'Unable to retrieve data for {symbol}')
+
+    bot.run(TOKEN)
+
+
+if __name__ == '__main__':
+    main()
---
+++ fetch_data.py
@@ -0,0 +1,63 @@
+import os
+
+from requests import Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+API_KEY = os.getenv("API_KEY")
+BASE_URL = os.getenv("BASE_URL")
+
+
+def fetch_price(**kwargs):
+    url = f"{BASE_URL}/quotes/latest"
+    parameters = {
+        "convert": "USD",
+    }
+
+    params = {**parameters, **kwargs}
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": API_KEY,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=params)
+        data = json.loads(response.text)
+        if "symbol" in params:
+            ticker = "symbol"
+        elif "id" in params:
+            ticker = "id"
+
+        price = round(data["data"][params[ticker]]["quote"]["USD"]["price"], 4)
+        return price
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
+
+def check_map(symbol):
+    url = f"{BASE_URL}/map"
+
+    params = {'symbol': symbol}
+
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": API_KEY,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=params)
+        data = json.loads(response.text)
+        symbol_map = data['data']
+        return symbol_map
+
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
+
+check_map('TIME')
